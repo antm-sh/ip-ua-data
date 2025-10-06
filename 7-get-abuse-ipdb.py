1181ac08b01e21ab2234f7f6f827c5312b53b86b
antm-sh <anthony.mitchinson@sitehost.nz>
2025-10-07 09:32:39 +1300

    Added Mitchell Krogza's list & de-duplicated

--- 7-get-abuse-ipdb.py
+++ 7-get-abuse-ipdb.py
@@ -9,9 +9,9 @@ OUTFILE = f"{ABUSE_IPS_DIR}/abusedb-ips"
 
 URL = "https://api.abuseipdb.com/api/v2/blacklist"
 LIMIT = str(10000)
+BAD_IPS_URL = "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/_generator_lists/bad-ip-addresses.list"
 
 API_KEY = os.getenv('ABUSE_IPDB_API_KEY')
-expiry_time = int((datetime.now() + timedelta(hours=4)).timestamp())
 
 querystring = {
     'limit': LIMIT
@@ -24,12 +24,28 @@ headers = {
 
 os.makedirs(ABUSE_IPS_DIR, exist_ok=True)
 
+# Collect all IPs in a set to deduplicate
+all_ips = set()
+
+# Fetch AbuseIPDB list
 response = requests.request(method='GET', url=URL, headers=headers, params=querystring)
 
+for line in response.text.splitlines():
+    ip = line.strip()
+    if ip:
+        all_ips.add(ip)
+
+# Fetch bad IP addresses list
+bad_ips_response = requests.get(BAD_IPS_URL)
+
+for line in bad_ips_response.text.splitlines():
+    ip = line.strip()
+    if ip:
+        all_ips.add(ip)
+
+# Write deduplicated IPs to file
 with open(OUTFILE, 'w') as f:
-    for line in response.text.splitlines():
-        if line.strip() == "":
-            continue
-        f.write(f'{line}\n')
+    for ip in sorted(all_ips):
+        f.write(f'{ip}\n')
 
 
